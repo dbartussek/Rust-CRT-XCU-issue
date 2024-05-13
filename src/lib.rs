8febe3caa8397c9cc26237c00d4a4de4ce238262
dario <d.bartussek@gmail.com>
2024-05-13 19:51:32 +0200

    Added another test method and "used_linker" feature to check

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 pub static DUMMY: AtomicBool = AtomicBool::new(false);
 
 #[ctor::ctor]
 fn dummy() {
     libc_print::libc_println!("lib");
+}
+
+#[inline(never)]
+pub fn touch_the_dummy() -> bool {
+    DUMMY.load(Ordering::SeqCst)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     if std::hint::black_box(false) {
         nested_init_test::DUMMY.load(std::sync::atomic::Ordering::SeqCst);
     }
+    #[cfg(feature = "dummy_touch_indirect")]
+    if std::hint::black_box(false) {
+        nested_init_test::touch_the_dummy();
+    }
 
     println!("Main");
 }
